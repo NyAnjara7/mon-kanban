8e224b445d98fe198c59ac0748ddfbe68411307d
Your Name <you@example.com>
2026-04-30 11:31:56 +0200

    Initialisation du projet Kanban

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return(
+    <div style={{ fontFamily:'sans-serif', padding:'2rem', textAlign:'center'}}>
+      <h1>Mon premier déploiement</h1>
+      <p>Projet réalisé par <strong>Ny Anjara Tiana</strong></p>
+      <p>Future application : gestion de tâches Kanban</p>
+    </div>
+  );
+}
+
+export default App
